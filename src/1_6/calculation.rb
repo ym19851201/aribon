d4ae1c5d137329d85a98dbd69f1cd59a7a10d2cd
miyazaki <miyazaki19851201@gmail.com>
2017-01-13 17:06:32 +0900

    Refactor

--- src/1_6/calculation.rb
+++ src/1_6/calculation.rb
@@ -5,18 +5,16 @@ def calculate(n, *args)
 
   sorted = args.flatten.sort.reverse
 
-  (0..sorted.length).each do |i|
+  (0..(sorted.length - 3)).each do |i|
     lengthes = sorted[i..i+2]
-    if lengthes.length < 3
-      return 0
-    end
-
     if triangle? lengthes
       return lengthes.inject :+
     end
   end
+  return 0
 end
 
 def triangle?(lengthes)
   lengthes[0] < lengthes[1] + lengthes[2]
 end
+
