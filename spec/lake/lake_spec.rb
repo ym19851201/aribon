c86b4e5336a90a60f341b6b8a1d76d764b8e52e0
miyazaki <miyazaki19851201@gmail.com>
2017-05-22 18:05:22 +0900

    Modify vain process within loop

--- spec/lake/lake_spec.rb
+++ spec/lake/lake_spec.rb
@@ -16,11 +16,15 @@ lake = [
 
 describe 'search_w' do
   it 'can search all "W" of lake' do
-    expect(search_w(lake, 0, 9)).to eq Set.new([[0, 9], [0, 10], [1, 9], [1, 10], [1, 11], [2, 9], [2, 10], [3, 9], [3, 10], [4, 9], [5, 9], [6, 9], [6, 10], [7, 10], [8, 10], [9, 10]])
+    expect(search_w(lake, 0, 9).sort).to eq [[0, 9], [0, 10], [1, 9], [1, 10], [1, 11], [2, 9], [2, 10], [3, 9], [3, 10], [4, 9], [5, 9], [6, 9], [6, 10], [7, 10], [8, 10], [9, 10]]
   end
 
   it 'can search all "W" of lake' do
-    expect(search_w(lake, 0, 0)).to eq Set.new([[0, 0], [1, 1], [1, 2], [1, 3], [2, 4], [2, 5]])
+    expect(search_w(lake, 0, 0).sort).to eq [[0, 0], [1, 1], [1, 2], [1, 3], [2, 4], [2, 5]]
+  end
+
+  it 'can search all "W" of lake' do
+    expect(search_w(lake, 5, 2).sort).to eq [[5, 2], [6, 1], [6, 3], [7, 0], [7, 2], [7, 4], [8, 1], [8, 3], [9, 2]]
   end
 end
 
--- src/lake/lake.rb
+++ src/lake/lake.rb
@@ -1,18 +1,15 @@
 require 'set'
 
-def search_w(lake, row, col, points=Set.new)
-  return if out_of_bounds? lake, row, col
-  return if points.include? [row, col]
+def search_w(lake, row, col, points = Set.new)
+  return if out_of_bounds?(lake, row, col)
   return unless lake[row][col] == 'W'
 
-  points.add [row, col] 
+  points.add [row, col]
 
-  search_w lake, row - 1, col + 1, points
-  search_w lake, row, col + 1, points
-  search_w lake, row + 1, col, points
-  search_w lake, row + 1, col + 1, points
-
-  search_w lake, row + 1, col - 1, points
+  search_w(lake, row, col + 1, points)
+  search_w(lake, row + 1, col, points)
+  search_w(lake, row + 1, col + 1, points)
+  search_w(lake, row + 1, col - 1, points)
 
   points
 end
@@ -21,15 +18,20 @@ def out_of_bounds?(lake, row, col)
   row < 0 || row > lake.length - 1 || col < 0 || col > lake[row].length - 1
 end
 
-def sets_has_item?(sets, item)
-  sets.any? {|s| s != nil && s.include?(item)}
+def searched?(lake, i, j)
+  w?(lake, i, j - 1) || w?(lake, i - 1, j - 1) ||
+    w?(lake, i - 1, j) || w?(lake, i - 1, j + 1)
+end
+
+def w?(lake, i, j)
+  !out_of_bounds?(lake, i, j) && lake[i][j] == 'W'
 end
 
 def collect_puddles(lake)
   puddles = []
-  lake.each.with_index do |row, i|
-    row.each.with_index do |col, j|
-      if !sets_has_item?(puddles, [i, j]) && lake[i][j] == 'W'
+  lake.each_index do |i|
+    lake[i].each_index do |j|
+      if lake[i][j] == 'W' && !searched?(lake, i, j)
         puddles << search_w(lake, i, j)
       end
     end
